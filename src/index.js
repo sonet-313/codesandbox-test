efb52c62e3f3c54c6dd9ae5a4d9f8dfcd2f9a018
Sonetaka Mikihiro <60766275+sonet-313@users.noreply.github.com>
2022-02-18 09:56:18 +0900

    default argument

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 /**
- * 分割代入
+ * デフォルト値、引数など
  */
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
 
-const myProfile = {
-  name: "そね",
-  age: 24
-};
-
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
-console.log(message1);
-
-const { name, age } = myProfile;
-const message2 = `名前は${name}です。年齢は${age}です。`;
-console.log(message2);
-
-const myProfileArray = ["そ", 24];
-
-const message3 = `名前は${myProfileArray[0]}です。年齢は${myProfileArray[1]}です。`;
-console.log(message3);
-
-const [nam, ag] = myProfileArray;
-const message4 = `名前は${nam}です。年齢は${ag}です。`;
-console.log(message4);
+sayHello();
+sayHello("sone");
