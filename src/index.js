327cef725352cdba6acdc2ed4e11dd98c753295b
Sonetaka Mikihiro <60766275+sonet-313@users.noreply.github.com>
2022-02-18 09:43:51 +0900

    arrow function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import "./styles.css";
+const func1 = function (str) {
+  return str;
+};
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+console.log(func1("func1です"));
+
+const func2 = (str) => {
+  return str;
+};
+
+console.log(func2("func2です"));
+
+const sum = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(sum(1, 2));
